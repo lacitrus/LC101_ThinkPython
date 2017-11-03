4487303b686a4403ad402a299c13a077eded1f2d
Yi <yizhang2@gmail.com>
2017-11-03 16:41:27 -0400

    editing done

--- initials/initials.py
+++ initials/initials.py
@@ -9,3 +9,11 @@ def get_initials(fullname):
         initials = initials + name[0].upper()  
 
     return initials
+
+def main():
+    fullname = input("What is your full name?")
+    print(get_initials(fullname))
+
+if __name__ == "__main__":
+    main()
+
