447ed7c2d1554bcd9ab9a7348d5e330bdbe52de8
Yi <yizhang2@gmail.com>
2017-11-19 18:20:18 -0500

    Finished caesar

---
+++ crypto/caesar.py
@@ -0,0 +1,32 @@
+def alphabet_position(letter):
+    
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    alphabet2 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    if letter in alphabet:
+        for i in range(len(alphabet)):
+            if letter == alphabet[i]:
+                return i
+    else:
+        for i in range(len(alphabet2)):
+            if letter == alphabet2[i]:
+                return i
+
+def rotate_character(char, rot):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    alphabet2 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    
+    if char in alphabet:
+        index = (alphabet_position(char) + rot ) % 26
+        return alphabet[index]
+    elif char in alphabet2:
+        index2 = (alphabet_position(char) + rot ) % 26
+        return alphabet2[index2]
+    else:
+        return char
+
+def encrypt(text, rot):
+    newtext = ''
+    for char in text:
+        newtext += rotate_character(char, rot)
+    return newtext
+
