fb83850cd1dd4df02946490fbb59bb6cf38d69a6
Yi Zhang <yizhang2@gmail.com>
2017-12-05 20:00:37 -0500

    ste

--- classnotes/12052017.py
+++ classnotes/12052017.py
@@ -2,4 +2,6 @@
 # Git remove
 # Git merge
 # Git pull --merge
-# git reset HEAD hard (will remove whatever changes and give you the stuff without the changes you made, things after the last push)
+# git reset HEAD hard (will remove whatever changes and give you the stuff without the changes you made, things after the last push)git 
+# git config user.name
+# git config user.email ( to show the user name and email account used for Git when you wanna check you are logged in, which is importantgit )
