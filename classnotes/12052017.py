87fff73e0bb61fea3132fbf17e866eaa917be2b6
Yi Zhang <yizhang2@gmail.com>
2017-12-05 19:29:11 -0500

    update

--- classnotes/12052017.py
+++ classnotes/12052017.py
@@ -1,1 +1,5 @@
 # Note for Unit #2
+# Git remove
+# Git merge
+# Git pull --merge
+# git reset HEAD hard (will remove whatever changes and give you the stuff without the changes you made, things after the last push)
