20382f8592ac17ee762391fda8eb86b5f22fa822
Yi Zhang <yizhang2@gmail.com>
2017-12-07 18:23:04 -0500

    changed note

--- classnotes/December2017.py
+++ classnotes/December2017.py
@@ -5,3 +5,6 @@
 # git reset HEAD hard (will remove whatever changes and give you the stuff without the changes you made, things after the last push)git 
 # git config user.name
 # git config user.email ( to show the user name and email account used for Git when you wanna check you are logged in, which is importantgit )
+
+
+# Dec 7, 2017 note 
